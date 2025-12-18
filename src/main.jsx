6bac73c9119776fdc46ba56c9899569a458c4420
Leonydis138 <138300852+Leonydis138@users.noreply.github.com>
2025-12-18 19:29:51 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import QuantumFlowOS from './QuantumFlowOS.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = document.getElementById("root");
+
+if (!root) {
+  throw new Error("Root element #root not found");
+}
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <QuantumFlowOS />
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
